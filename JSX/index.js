bb200528365b2cb698030956971e4e4a54450015
Makhmud <makhmudgaly2@gmail.com>
2020-03-13 11:23:48 +0700

    JSX practice

---
+++ JSX/index.js
@@ -0,0 +1,9 @@
+class JSXDemo extends React.Component {
+  render() {
+      return (
+          <img src="https://images.unsplash.com/photo-1583936078271-16a2d06a41af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80"></img>
+      )
+  }
+} 
+
+ReactDOM.render(<JSXDemo />, document.getElementById('root'))
